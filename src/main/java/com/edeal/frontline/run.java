ee121f65b28c398b93a56c074246f629c17e80e5
a.bernard <a.bernard@e-deal.com>
2016-07-21 14:51:29 +0200

    commit

--- src/main/java/com/edeal/frontline/run.java
+++ src/main/java/com/edeal/frontline/run.java
@@ -1,22 +1,57 @@
 package com.edeal.frontline;
 
 import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.ContainerAwareDetector;
+import org.apache.tika.detect.Detector;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class run {
 
-
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\a.bernard\\Desktop\\test\\docx");
+		File file = new File("C:\\Users\\a.bernard\\Desktop\\test\\");
+		try {
+			TikaConfig tika = TikaConfig.getDefaultConfig();;
+			for (File temp : file.listFiles()) {
+
+
+
+
+				MediaType mediaType;
+				Metadata md = new Metadata();
+				md.set(Metadata.RESOURCE_NAME_KEY, temp.getName());
+				Detector detector = new ContainerAwareDetector(tika.getMimeRepository());
+
+				try {
+					mediaType = detector.detect(TikaInputStream.get(temp), md);
+					String mimeType = mediaType.toString();
+					System.out.println(mimeType);
+				} catch (IOException ioe) {
+					System.out.println("yolo");
+				}
+
+
+/*				String mimeType = mediaType.getSubtype();
+
+				Metadata metadata = new Metadata();
+				metadata.set(Metadata.RESOURCE_NAME_KEY, file.toString());
+				Parser parser = new AutoDetectParser();
+
+				ContentHandler contenthandler = new BodyContentHandler();
+				parser.parse(TikaInputStream.get(temp),contenthandler,  metadata);
+				System.out.println("Mime: " + metadata.get(Metadata.CONTENT_TYPE));*/
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 /*		AutoDetectParser parser = new AutoDetectParser();
 		BodyContentHandler handler = new BodyContentHandler();
@@ -37,20 +72,5 @@ public class run {
 					TikaInputStream.get(new FileInputStream(file)), metadata);
 			System.out.println("File is " + mimetype.toString());
 */
-
-
-		try {
-			TikaConfig tika = new TikaConfig();
-			Metadata metadata = new Metadata();
-			metadata.set(Metadata.RESOURCE_NAME_KEY, file.toString());
-			MediaType mimetype = tika.getDetector().detect(
-					TikaInputStream.get(file), metadata);
-			System.out.println("File " + file + " is " + mimetype);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-
-
 	}
 }
